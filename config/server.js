1c5b7a1835e3d1be610ec9866eee009515a21eff
Charles Bourtoire <charlesbourtoire@gmail.com>
2021-11-29 12:36:53 +0100

    bases api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c4ea6db6d871ff723c71fe64794d32fa'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a284d5d6-5d06-4cc4-8adc-c4d0e7e0748a'
+};
